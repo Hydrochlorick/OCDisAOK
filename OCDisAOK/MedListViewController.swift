a14ce3d96eaa5741caeed5d7fe899cf9b8e7daa7
Hydrochlorick <hydrochlorick@icloud.com>
2021-01-12 13:00:41 -0800

    Well I don't remember 2012, but I heard the world would turn to hell. And compared to that I'm doing swell. So I kinda hope it really did.

--- OCDisAOK/MedListViewController.swift
+++ OCDisAOK/MedListViewController.swift
@@ -11,19 +11,39 @@ import UIKit
 class MedListViewController: UITableViewController {
     var medications = [Medication]()
 
+    @IBOutlet var MedTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Tell the table to use this... file?/class/idk as its data source
+        MedTableView.dataSource = self
+        
+        // Create two dummy meds to put in the array to see if it works
+        let med1 = Medication(name: "Chicken Nuggets", dose: 100, units: .miligram)
+        let med2 = Medication(name: "Alcohol", dose: 400, units: .milliliter)
+        
+        medications.append(med1)
+        medications.append(med2)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
         return medications.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "medCell", for: indexPath)
-        cell.textLabel?.text = "Drug Name"
-        cell.detailTextLabel?.text = "Dosage"
+        let med = medications[indexPath.row]
+        
+        let dose = "\(med.dose.description)\(med.units.rawValue)"
+        
+        cell.textLabel?.text = med.name
+        cell.detailTextLabel?.text = dose
         return cell
     }
     
